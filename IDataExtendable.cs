25931206c0ccdc6ccb2a12b1a0c4dc0caa83cf50
Benedikt Papiorek <benedikt@papiorek.de>
2025-03-08 06:43:43 +0100

    Added Support for .NET Standard 2.0

--- IDataExtendable.cs
+++ IDataExtendable.cs
@@ -14,24 +14,24 @@ namespace MyDevTime.Interfaces.DataExtendable
         /// <summary>
         /// Is used to store all Extensions identified by their ExtensionId.
         /// </summary>
-        public ICollection<T> Extensions { get; set; }
+        ICollection<T> Extensions { get; set; }
 
         #endregion
-
-
+        
+        
         #region DataExtendableFunctions
 
         /// <summary>
         /// Adds a new data extension to the object.
         /// </summary>
         /// <param name="dataExtension">The data extension to add.</param>
-        public bool AddDataExtension(T dataExtension);
+        bool AddDataExtension(T dataExtension);
 
         /// <summary>
         /// Removes an existing data extension from the object.
         /// </summary>
         /// <param name="dataExtension">The data extension to remove.</param>
-        public bool RemoveDataExtension(T dataExtension);
+        bool RemoveDataExtension(T dataExtension);
 
         /// <summary>
         /// Retrieves a data extension by its unique identifier.
@@ -39,8 +39,8 @@ namespace MyDevTime.Interfaces.DataExtendable
         /// <param name="extensionId">The id of the extension.</param>
         /// <param name="dataExtension">The requested extension or null</param>
         /// <returns>True if an extension was found otherwise false.</returns>
-        public bool GetDataExtension(string extensionId, [NotNullWhen(true)] out T? dataExtension);
-
+        bool GetDataExtension(string extensionId, out T dataExtension);
+        
         #endregion
     }
 }

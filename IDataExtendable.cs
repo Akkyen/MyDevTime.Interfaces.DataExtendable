34d672010f7db0a6e4bda7ad93a2ad87c1a1be2b
Benedikt Papiorek <benedikt@papiorek.de>
2025-03-17 13:13:24 +0100

    Removed properties because in my opinion interfaces should more behave like traits in rust and not define data

--- IDataExtendable.cs
+++ IDataExtendable.cs
@@ -1,45 +1,13 @@
-﻿using System.Collections.Generic;
-
-namespace MyDevTime.Interfaces.DataExtendable
+﻿namespace MyDevTime.Interfaces.DataExtendable
 {
-    /// <summary>
-    /// Defines a contract for objects that support the addition, removal, and retrieval of data extensions.
-    /// </summary>
-    /// <typeparam name="T">The type of the extension. Needs to implement <see cref="IDataExtension"/></typeparam>
     public interface IDataExtendable<T>
         where T : class, IDataExtension
     {
-        #region Fields and Properties
-
-        /// <summary>
-        /// Stores all extensions.
-        /// </summary>
-        ICollection<T> Extensions { get; set; }
-
-        #endregion
-        
-        
         #region DataExtendableFunctions
-
-        /// <summary>
-        /// Adds a new data extension to the object implementing IDataExtendable.
-        /// </summary>
-        /// <param name="dataExtension">The data extension to add.</param>
+        
         bool AddDataExtension(T dataExtension);
-
-        /// <summary>
-        /// Removes an existing data extension from the object implementing IDataExtendable.
-        /// </summary>
-        /// <param name="dataExtension">The data extension to remove.</param>
+        
         bool RemoveDataExtension(T dataExtension);
-
-        /// <summary>
-        /// Retrieves a data extension by its unique identifier.
-        /// </summary>
-        /// <param name="extensionId">The id of the extension.</param>
-        /// <param name="dataExtension">The requested extension or null</param>
-        /// <returns>True if an extension was found otherwise false.</returns>
-        bool GetDataExtension(string extensionId, out T dataExtension);
         
         #endregion
     }
--- IDataExtension.cs
+++ IDataExtension.cs
@@ -1,17 +1,6 @@
 namespace MyDevTime.Interfaces.DataExtendable
 {
-    /// <summary>
-    /// Represents an individual data extension that can be added to an extendable object.
-    /// </summary>
     public interface IDataExtension
     {
-        #region Fields and Properties
-
-        /// <summary>
-        /// The id of the extension.
-        /// </summary>
-        string ExtensionId { get; set; }
-
-        #endregion
     }
 }
